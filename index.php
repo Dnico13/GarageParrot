410cd8ad507136e8be3110172aa08ce13b1e0671
Dnico13 <Dnico13@hotmail.com>
2023-08-02 23:52:04 +0200

    debut projet

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
     <title>Garage Parot: Garage automobile dans la région toulousaine.</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 
 </head>
 <body>
@@ -20,12 +21,11 @@
 
 
     </header>
-    <main>
-        <article>
-
-
-        </article>
-
+    <main class="containerAcceuil">
+        <a href="welcome.php">
+            <img src="./asset/iconeGarageParrot.jpg" alt="Logo du Garage  Vincent Parrot" class="logoPic animate__animated animate__backInDown">
+        </a>
+        <h1 class="titleBeginn animate__animated animate__fadeInUpBig animate__delay-1s animate__repeat-1 	"> Garage Vincent Parrot </h1>
 
     </main>
     <footer>
