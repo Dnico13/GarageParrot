6d4a348ad852055274e98fe4c340b685d9011f23
Dnico13 <Dnico13@hotmail.com>
2023-09-10 23:14:25 +0200

    contact avec formulaire ok

--- contact.php
+++ contact.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,59 +16,81 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 
 </head>
+
 <body>
     <header>
-            <?php
-                require_once './template/header.php';
-            ?> 
-            <h1> Formulaire de contact</h1>       
+        <?php
+        require_once './template/header.php';
+        ?>
+        <h1> Contactez-nous</h1>
     </header>
-    <main class="formcontact1">
-            <form class="formcontact" method="post" action="envoiContact.php">
-                <div class="divformcontact">
-                    <label for="firstname">Prénom: </label>
-                        <div>
-                            <input type="text" name="firstname" id="firstname" require><br>
+    <main>
+        <div class=" container  text-center d-flex flex-column my-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <p>Vous voulez prendre un rdv, obtenir un devis,
+                        avoir un renseignement?</p>
+                    <p>Nos equipes sont là pour répondre à vos attentes.</p>
+                    <p>Contactez-nous par télèphone:</p>
+                    <p><a href="tel:0605060704">06 05 06 07 04</a></p>
+                    <P>Ou remplissez le formulaire afin que nous puissions revenir vers
+                        vous.</P>
+                    <p>A tres vite..</p>
+
+                </div>
+                <div class="col-md-8 d-flex flex-column">
+
+                    <form class="formcontact" method="post" action="envoiContact.php">
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <label for="firstname">Prénom: </label>
+                                <div>
+                                    <input type="text" name="firstname" id="firstname" class="w-75" require><br>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="name">Nom:</label>
+                                <div>
+                                    <input type="text" name="name" id="name" require class="w-75"><br>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="email">Email:</label>
+                                <div>
+                                    <input type="email" name="email" id="email" require class="w-75"><br>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 ">
+                                <label for="phone"> Tel:</label>
+                                <div>
+                                    <input type="tel" name="phone" id="phone" require minlength="10" maxlength="10" class="w-75"><br>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="commentaire"></label>
+
+                                <textarea name="commentaire" id="commentaire" class="col-md-8" cols="28" rows="5" require>Message...</textarea><br>
+
+                            </div>
+                            <button type="submit" class="validcontact mb-1 mx-auto w-25 align-self-end" name="submit">Envoyer</button>
                         </div>
-                </div>
 
-                <div class="divformcontact">
-                    <label for="name">Nom:</label>
-                        <div>
-                            <input type="text" name="name" id="name" require><br>
-                        </div>              
+                    </form>
                 </div>
-
-                <div class="divformcontact">
-                    <label for="email">Email:</label>
-                        <div>
-                            <input type="email" name="email" id="email" require><br>
-                        </div>
-                 </div>
-
-                <div class="divformcontact">
-                    <label for="phone"> Tel:</label>
-                        <div>    
-                            <input type="tel" name="phone" id="phone" require minlength="10" maxlength="10"><br>
-                        </div>             
-                </div>
-                <div class="divformcontact">
-                    <label for="commentaire"></label>
-                        
-                            <textarea name="commentaire" id="commentaire" cols="28" rows="5" require>Message...</textarea><br>   
-                        
-                </div>
-                <button  type="submit" class="validcontact" name="submit">Envoyer</button>
-            </form>
+            </div>
+        </div>
 
     </main>
     <footer>
-            <?php
-                require_once './template/footer.php';
-            ?>
+        <?php
+        require_once './template/footer.php';
+        ?>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="app.js"></script>
 </body>
-</html>   
-    
+
+</html>
